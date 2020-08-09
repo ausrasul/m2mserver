ed859e5cf4f9d94da42e3a8e3d9882e88950878b
Aus Abdalrasul <aus.rasul@mobilearts.com>
2020-08-09 14:50:16 +0200

    Add data size to help reframing when received by client

--- trx.go
+++ trx.go
@@ -5,9 +5,10 @@ import (
 	"time"
 	"bufio"
 	"encoding/json"
+	"strconv"
 )
 
-
+/*
 func sender(conn net.Conn, outbox <-chan Cmd, stopSend <-chan bool, sendStopped chan<- bool){
 	for {
 		select{
@@ -22,12 +23,17 @@ func sender(conn net.Conn, outbox <-chan Cmd, stopSend <-chan bool, sendStopped 
 				sendStopped <- true
 				return
 			}
-			w   := bufio.NewWriter(conn)
-			w = bufio.NewWriterSize(w, len(msg) + 10)
+			//w   := bufio.NewWriter(conn)
+			w := bufio.NewWriterSize(conn, len(msg) + 1)
 
 			bytesSent, err := w.Write(append(msg, byte('\n')))
-			w.Flush()
 			if err != nil || bytesSent < len(msg) + 1{
+				log.Print("Error buffering msg")
+				sendStopped <- true
+				return
+			}
+			err = w.Flush()
+			if err != nil {
 				log.Print("Error sending msg")
 				sendStopped <- true
 				return
@@ -36,7 +42,59 @@ func sender(conn net.Conn, outbox <-chan Cmd, stopSend <-chan bool, sendStopped 
 		}
 	}
 }
+*/
+func sender(conn net.Conn, outbox <-chan Cmd, stopSend <-chan bool, sendStopped chan<- bool){
+	for {
+		select{
+		case <- stopSend:
+			sendStopped <- true
+			log.Print("sender stopping.")
+			return
+		case command:= <- outbox :
+			msg, err := json.Marshal(command)
+			if err != nil {
+				log.Print("Error marshaling msg")
+				sendStopped <- true
+				return
+			}
+			// sending message header, the data size.
+			dataSize := []byte(strconv.Itoa(len(msg)))
+			dataSize = append(dataSize, byte('\n'))
+			w := bufio.NewWriterSize(conn, len(dataSize))
 
+			bytesSent, err := w.Write(dataSize)
+			if err != nil || bytesSent < len(dataSize) {
+				log.Print("Error buffering msg len")
+				sendStopped <- true
+				return
+			}
+			err = w.Flush()
+			if err != nil {
+				log.Print("Error sending msg")
+				sendStopped <- true
+				return
+			}
+			log.Print("bytes sent (header): ", bytesSent)
+
+			// sending the data
+			w = bufio.NewWriterSize(conn, len(msg))
+
+			bytesSent, err = w.Write(msg)
+			if err != nil || bytesSent < len(msg){
+				log.Print("Error buffering msg")
+				sendStopped <- true
+				return
+			}
+			err = w.Flush()
+			if err != nil {
+				log.Print("Error sending msg")
+				sendStopped <- true
+				return
+			}
+			log.Print("bytes sent: ", bytesSent)
+		}
+	}
+}
 
 func receiver(conn net.Conn, inbox chan<- Cmd, stopRcv <-chan bool, rcvStopped chan<- bool){
 	for {
@@ -48,7 +106,7 @@ func receiver(conn net.Conn, inbox chan<- Cmd, stopRcv <-chan bool, rcvStopped c
 		default:
 		}
 		conn.SetDeadline(time.Now().Add(1e9))
-		buf, err := bufio.NewReader(conn).ReadString('\n')
+		buf, err := bufio.NewReaderSize(conn, 65537).ReadString('\n')
 		if err != nil {
 			if opErr, ok := err.(*net.OpError); ok && opErr.Timeout() {
 				continue
