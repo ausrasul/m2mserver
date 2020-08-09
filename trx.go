dfe3a8dd1017405a38e04d3fff2b392ce21b9f00
Aus Abdalrasul <aus.rasul@mobilearts.com>
2020-08-09 23:01:28 +0200

    Add data length to defragment received packets

--- trx.go
+++ trx.go
@@ -8,41 +8,6 @@ import (
 	"strconv"
 )
 
-/*
-func sender(conn net.Conn, outbox <-chan Cmd, stopSend <-chan bool, sendStopped chan<- bool){
-	for {
-		select{
-		case <- stopSend:
-			sendStopped <- true
-			log.Print("sender stopping.")
-			return
-		case command:= <- outbox :
-			msg, err := json.Marshal(command)
-			if err != nil {
-				log.Print("Error marshaling msg")
-				sendStopped <- true
-				return
-			}
-			//w   := bufio.NewWriter(conn)
-			w := bufio.NewWriterSize(conn, len(msg) + 1)
-
-			bytesSent, err := w.Write(append(msg, byte('\n')))
-			if err != nil || bytesSent < len(msg) + 1{
-				log.Print("Error buffering msg")
-				sendStopped <- true
-				return
-			}
-			err = w.Flush()
-			if err != nil {
-				log.Print("Error sending msg")
-				sendStopped <- true
-				return
-			}
-			log.Print("bytes sent: ", bytesSent)
-		}
-	}
-}
-*/
 func sender(conn net.Conn, outbox <-chan Cmd, stopSend <-chan bool, sendStopped chan<- bool){
 	for {
 		select{
@@ -96,7 +61,11 @@ func sender(conn net.Conn, outbox <-chan Cmd, stopSend <-chan bool, sendStopped 
 	}
 }
 
+
 func receiver(conn net.Conn, inbox chan<- Cmd, stopRcv <-chan bool, rcvStopped chan<- bool){
+	chunkSize := 4*1024
+	// don't recreate the reader buffer, or else you loose unread buffered data.
+	r := bufio.NewReaderSize(conn, chunkSize)
 	for {
 		select {
 		case <- stopRcv:
@@ -105,19 +74,68 @@ func receiver(conn net.Conn, inbox chan<- Cmd, stopRcv <-chan bool, rcvStopped c
 			return
 		default:
 		}
+
 		conn.SetDeadline(time.Now().Add(1e9))
-		buf, err := bufio.NewReaderSize(conn, 65537).ReadString('\n')
+		// read a chunk and extract following data size to allow us to reassemble the frames.
+		dataSizeByte, err := r.ReadBytes('\n')
 		if err != nil {
+			// expect reader to timeout and re run the loop when no data is in queue.
 			if opErr, ok := err.(*net.OpError); ok && opErr.Timeout() {
 				continue
 			}
 			log.Print("Error reading:", err.Error())
-			rcvStopped <- true
 			return
+			rcvStopped <- true
 		}
-		log.Print(":", buf)
+		dataSizeByte = dataSizeByte[:len(dataSizeByte)-1] // strip the last byte \n
+		dataSize, err := strconv.Atoi(string(dataSizeByte))
+		if err != nil {
+			log.Print("data size not integer ", dataSizeByte, err)
+			continue
+		}
+		log.Print("dataSize ", dataSize)
+		// holder for data
+		data := make([]byte, 0)
+		for {
+			// read only enough data, less or equal to chunkSize, but not too much
+			// risking reading data that doesn't belong to us.
+			buffSize := dataSize
+			if dataSize > chunkSize{
+				buffSize = chunkSize
+			}
+			if buffSize == 0 {
+				log.Print("data read complete.")
+				break
+			}
+			log.Print("dataSize: ", dataSize, "buffSize ", buffSize)
+			buf := make([]byte, buffSize) //the chunk size
+			n, err := r.Read(buf) //loading chunk into buffer
+			log.Print("read ", n, " bytes.")
+			// update the remaining bytes we expect to read.
+			dataSize -= n
+			buf = buf[:n] // trim the read buffer
+			if n == 0 || err != nil{
+				if err == io.EOF {
+					// this will probably never be the case.
+					log.Print("read complete")
+					break
+				} else if neterr, ok := err.(net.Error); ok && neterr.Timeout() {
+					log.Print("read time out")
+					break
+				} else if err != nil {
+					log.Print("very bad error ", n, err)
+					break
+				}
+				log.Print("unknown error ", n, err)
+				break
+			} else {
+				// reassemble the data.
+				data = append(data, buf...)
+			}
+		}
+
 		var cmd Cmd
-		err = json.Unmarshal ([]byte(buf) , &cmd)
+		err = json.Unmarshal (data , &cmd)
 		if err != nil {
 			log.Print("Error unmarshaling msg:", err)
 			rcvStopped <- true
